ac184c644d27faaa4157586766c557c8f839f3ef
Mike <migueligr2000@gmail.com>
2024-01-15 05:05:28 +0100

    -Tailwind initial set-up -Initial data retrieval -Styling

--- src/components/pet-overview/pet-overview.component.ts
+++ src/components/pet-overview/pet-overview.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { pet } from '../../models/pet.model';
+import { PetService } from '../../services/pet.service';
 
 @Component({
   selector: 'app-pet-overview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-overview.component.html',
   styleUrl: './pet-overview.component.scss'
 })
-export class PetOverviewComponent {
+export class PetOverviewComponent implements OnInit{
+
+  
+  petService = inject(PetService);
+  
+  petList$: Observable<pet[]> | undefined;
+
+  ngOnInit(): void {
+    this.petList$ =  this.petService.getPetsByStatus();
+  }
+
+  
 
 }
---
+++ src/services/pet.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { pet } from '../models/pet.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+
+  httpClient = inject(HttpClient);
+
+  getPetsByStatus(status:string = 'available'): Observable<pet[]>{
+    return this.httpClient.get('https://petstore3.swagger.io/api/v3/pet/findByStatus', {params: {status}}) as Observable<pet[]>
+  }
+}
